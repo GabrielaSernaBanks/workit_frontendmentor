d08128e30d36ab9f591a5a8ce8f01d87003d2273
GabrielaSernaBanks <gserna88@gmail.com>
2023-07-18 22:26:18 -0400

    Editing text in nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,14 +4,25 @@ import Logo from '../assets/images/logo-light.svg'
 function Nav() {
 	return (
 		<>
-		<header>
-			<nav className='nav'>
-				<a className='nav__logo'><img src={Logo}></img></a>
-				<div className='nav__link'>
-					<p>Apply for access</p>
-				</div>
-			</nav>
-		</header>
+			<header className='header'>
+				<nav className='nav'>
+					<div>
+						<a><img className='nav__logo' src={Logo}></img></a>
+					</div>
+					<div className='nav__link'>
+						<p>Apply for access</p>
+					</div>
+				</nav>
+					<div className='nav-info'>
+						<h1 className='nav-info__text'>
+							Data tailored to<br></br> your needs.
+						</h1>
+						<button className='nav-info__button'>Learn more</button>
+					</div>
+
+			</header>
+
+
 		</>
 	);
 }
